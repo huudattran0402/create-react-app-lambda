5470238951058917648f12dca89046792197f874
huudattran0402 <81469596+huudattran0402@users.noreply.github.com>
2022-12-16 11:34:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,10 +37,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
         </header>
         <h1> TRẦN HỮU DẠT </h1>
       </div>
